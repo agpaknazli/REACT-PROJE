6cc4091ba54310d935c436695aa2c44127035f76
agpaknazli <agpaknazli19@gmail.com>
2024-08-24 10:29:46 +0200

    pro2

--- react pro2/src/components/header/Header.jsx
+++ react pro2/src/components/header/Header.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import "./Header.scss"
+
+
+
+
 const Header = () => {
+
+
+
+  const categories= ["All","Electronics", 'Jewelery', "Man's Clothing", "Women's Clothing"]
   return (
     <div>
 <h1 className='header'>Product List</h1>
 
-<div>
+<div className='btnWrapper'>
+{
+categories.map((category,i)=> <button type='submit' 
+className='btn'
+key={i}> {category} </button>)
 
-<div className='btn'>  </div>
+
+}
 </div>
 
     </div>
