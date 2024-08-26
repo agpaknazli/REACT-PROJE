0e6fbd480dd957e8b27659532b871bb13bd6052e
agpaknazli <agpaknazli19@gmail.com>
2024-08-26 18:13:32 +0200

    img

--- react-pro2/src/helper/data.js
+++ react-pro2/src/helper/data.js
@@ -6,7 +6,7 @@ export const products = [
     description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    image: "https://cdn.pixabay.com/photo/2016/11/23/18/12/bag-1854148_1280.jpg",
     rating: {
       rate: 3.9,
       count: 120,
@@ -20,7 +20,7 @@ export const products = [
       "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
     category: "men's clothing",
     image:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      "https://cdn.pixabay.com/photo/2017/05/23/10/53/t-shirt-design-2336850_640.jpg",
     rating: {
       rate: 4.1,
       count: 259,
@@ -33,7 +33,7 @@ export const products = [
     description:
       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    image: "https://cdn.pixabay.com/photo/2020/07/29/02/34/man-5446737_640.jpg",
     rating: {
       rate: 4.7,
       count: 500,
@@ -193,7 +193,7 @@ export const products = [
     description:
       "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+    image: "https://cdn.pixabay.com/photo/2015/10/08/10/03/clothing-977417_1280.jpg",
     rating: {
       rate: 2.6,
       count: 235,
