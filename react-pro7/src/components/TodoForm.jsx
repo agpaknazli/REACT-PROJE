a1bf8628a9663c23dd6f9e032daec92cd8fe368f
agpaknazli <agpaknazli19@gmail.com>
2024-08-30 23:41:36 +0200

    icon

--- react-pro7/src/components/TodoForm.jsx
+++ react-pro7/src/components/TodoForm.jsx
@@ -11,8 +11,8 @@ const handleSubmit=(e)=>{
 
   return (
     <form className='TodoForm' onSubmit={handleSubmit} >
-    <input className='todo-input' value={value} onChange={(e)=>setValue(e.target.value)
-    } placeholder='Enter Your Todo' type="text" name='todo' id='todo' />
+    <input className='todo-input placeholder:text-sclate-400' value={value} onChange={(e)=>setValue(e.target.value)
+    }  placeholder='Enter Your Todo' type="text" name='todo' id='todo' />
 
     <button  type="submit" className='todo-btn'>Add New Task</button>
         
