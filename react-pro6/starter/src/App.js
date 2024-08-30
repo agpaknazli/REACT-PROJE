947d949fa703dcfdfae9e4f0ff6c8ca561dc1350
agpaknazli <agpaknazli19@gmail.com>
2024-08-30 19:22:12 +0200

    react-pro7

--- react-pro6/starter/src/App.js
+++ react-pro6/starter/src/App.js
@@ -1,11 +1,15 @@
-
-
+import Container from "react-bootstrap/Container"
+import Header from "./componets/header/Header"
+import LegendContainer from "./components/legends/LegendContainer";
 function App() {
   return (
-    <div>
-      Hello React
-    </div>
+    // <div className="container">
+    //   Hello React
+    // </div>
+    <Container className="text-center my-5">
+      <Header />
+      <LegendContainer />
+    </Container>
   );
 }
-
 export default App;
