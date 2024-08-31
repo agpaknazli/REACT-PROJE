4dcce5ecaac2ba0837ce63d8f7698ff627f07486
agpaknazli <agpaknazli19@gmail.com>
2024-08-31 12:47:30 +0200

    inputtext

--- react-pro7/src/components/TodoForm.jsx
+++ react-pro7/src/components/TodoForm.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import TodoItem from './TodoItem';
 
 
 const TodoForm = () => {
-const[value, setValue]=useState("")
-
+const[value, setValue]=useState("");
+const data=useRef();
 const handleSubmit=(e)=>{
-    e.preventDefault();
+  e.preventDefault();
+  const İnputText=data.current.value.trim();
+  if(İnputText===""){
+    return null;
+
+  }
+  data.current.value="";
+    
+console.log(İnputText)
+
 }
 
 
 
   return (
     <form className='TodoForm' onSubmit={handleSubmit} >
-    <input className='todo-input placeholder:text-sclate-400' value={value} onChange={(e)=>setValue(e.target.value)
-    }  placeholder='Enter Your Todo' type="text" name='todo' id='todo' />
+    <input ref={data} className='todo-input placeholder:text-sclate-400' 
+    
+    
+    // value={value} onChange={(e)=>setValue(e.target.value.trim())}
+    
+    placeholder='Enter Your Todo' type="text" name='todo' id='todo' />
 
-    <button  type="submit" className='todo-btn'>Add New Task</button>
+    <button  type="submit" onChange={()=>handleSubmit()} className='todo-btn'>Add New Task</button>
 
 
 
